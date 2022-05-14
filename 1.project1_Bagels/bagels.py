2da1561fdd88b713dccfb46adfaf57e2ebb38ca5
j0zeph <notaj@tutanota.de>
2022-05-15 01:16:09 +0300

    Add description for bagels

---
+++ 1.project1_Bagels/bagels.py
@@ -0,0 +1,8 @@
+"""
+Bagels is a guessing game in which the user guesses a 3-digit number.
+
+The program prints:
+"Pico" for each correct number in the guess that is in the wrong place,
+"Fermi" for each correct number in the guess that is the right place,
+"Bagels" if none of the digits is correct.
+"""
