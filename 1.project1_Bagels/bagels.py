24375659215fd3c719ff354b0fb7c91f1719453c
j0zeph <notaj@tutanota.de>
2022-05-15 01:16:54 +0300

    Prompt the user for guesses

--- 1.project1_Bagels/bagels.py
+++ 1.project1_Bagels/bagels.py
@@ -6,3 +6,19 @@ The program prints:
 "Fermi" for each correct number in the guess that is the right place,
 "Bagels" if none of the digits is correct.
 """
+
+import random
+from textwrap import dedent
+
+MAX_GUESSES = 10
+
+# Accept leading zeroes as well.
+computer_num = str(random.randint(0, 999)).zfill(3)
+
+print(dedent('''
+    I am thinking of a number. 
+    You have 10 guesses to get it.
+    '''))
+
+for guess in range(MAX_GUESSES):
+    user_num = input(f'Guess #{guess}:\n>')
