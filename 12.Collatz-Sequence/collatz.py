1ac62ea736ce6868a6f1651d105925845d08a98c
j0zeph <notaj@tutanota.de>
2023-01-12 02:55:02 +0300

    Add visualization to the Collatz numbers
    Instead of printing all the Collatz numbers at once, they are now
    printed one-by-one with a visual indicator (using arrows) to show
    the difference between consecutive numbers.

--- 12.Collatz-Sequence/collatz.py
+++ 12.Collatz-Sequence/collatz.py
@@ -1,25 +1,52 @@
 """Program that generates numbers from the Collatz Sequence,
 from the user-provided number,n, up to 1"""
 
+import time
+from collections import deque
+import rich
+
+UP_ARROW = '\u2B08'
+DOWN_ARROW = '\u2B0A'
+LEFT_BRACKET = '\u27E6'
+RIGHT_BRACKET = '\u27E7'
+LONG_WAIT = 0.8
+SHORT_WAIT = 0.3
+
 
 def main():
     while True:
         start_num = int(input("Enter a starting number (greater than 0) or QUIT:\n> "))
         if start_num <= 0:
             continue
+        break
+
+    pairs = deque()
+
+    for i, num in enumerate(get_collatz_numbers(start_num)):
+        # show the starting number as well
+        if i == 0:
+            print(f'{LEFT_BRACKET}{num}{RIGHT_BRACKET} ', end='', flush=True)
+
+        pairs.append(num)
+
+        # only get arrows if there is a pair of numbers to look at
+        if not len(pairs) == 2:
+            continue
         else:
-            sequence = get_collatz(start_num)
-            print(sequence)
-            break
+            prev_num = pairs[0]
+            current_num = pairs[1]
+            arrows = get_arrows(prev_num, current_num)
+            time.sleep(LONG_WAIT)
+            rich.print(f'{arrows} ', end='', flush=True)
+            time.sleep(SHORT_WAIT)
+            print(f'{LEFT_BRACKET}{current_num}{RIGHT_BRACKET} ', end='', flush=True)
+            pairs.popleft()
 
 
-def get_collatz(n):
-    """Returns a collatz sequence from n to 1"""
-    
-    collatz_seq = []
-
+def get_collatz_numbers(n):
+    """Returns collatz numbers, one at a time, from n to 1"""
     # keep the first number as well
-    collatz_seq.append(str(n))
+    yield n
 
     while True:
         if n == 1:
@@ -27,13 +54,23 @@ def get_collatz(n):
         elif n % 2 == 0:
             # for the even case
             n = n // 2
-            collatz_seq.append(str(n))
+            yield n
         else:
             # for the odd case
             n = n * 3 + 1
-            collatz_seq.append(str(n))
+            yield n
 
-    return ', '.join(collatz_seq)
+
+def get_arrows(prev_num, current_num) -> str:
+    """Returns how many arrows (representing the absolute difference) there are between two numbers.
+    Upward facing arrows represent an increase between the two numbers.
+    Downward facing arrows represent a decrease between the two numbers."""
+
+    arrow_count = abs(current_num - prev_num)
+    if current_num < prev_num:
+        return '[bold deep_pink4]{}[/bold deep_pink4]'.format(DOWN_ARROW * arrow_count)
+    else:
+        return '[bold green]{}[/bold green]'.format(UP_ARROW * arrow_count)
 
 
 if __name__ == "__main__":
