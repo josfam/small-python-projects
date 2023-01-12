8574351e633bfa3f5bdf66f2d1c8fc98b465c8b7
j0zeph <notaj@tutanota.de>
2023-01-12 15:39:14 +0300

    Add rich formating to progress bar
    Rich formatting including colour, has been added to the
    progress bar.
    
    'Data' that is yet to be downloaded is now represented by a
    dash, and not an empty space.

--- 57.Progress-Bar/progress.py
+++ 57.Progress-Bar/progress.py
@@ -1,10 +1,12 @@
 """Program that simulates a download progress bar on the commandline"""
 
 import random
+import rich
 import time
 
 PAUSE_DURATION = 0.2
-ONE_BAR = chr(9608)  # 9608 represents '█'
+ONE_BAR = chr(9608)  # represents █
+ONE_DASH = '\u2013' # represents –
 BAR_WIDTH = 40
 DOWNLOAD_SIZE = 4096
 
@@ -14,19 +16,19 @@ def main():
     bytes_downloaded = 0
 
     while bytes_downloaded < DOWNLOAD_SIZE:
-        # "download" a random number of "bytes", then show the progress bar
+        # 'download' a random number of 'bytes', then show the progress bar
         bytes_downloaded += random.randint(0, 100)
 
-        # clamp down the "bytes" "downloaded" to stay between 0 and the total bytes
+        # clamp down the 'bytes' 'downloaded' to stay between 0 and the total bytes
         if bytes_downloaded > DOWNLOAD_SIZE:
             bytes_downloaded = DOWNLOAD_SIZE
 
         progress_bar = get_progress_bar(bytes_downloaded, DOWNLOAD_SIZE)
-        print(progress_bar, end='', flush=True)
+        rich.print(progress_bar, end='', flush=True)
 
-        # pause for a while
         time.sleep(PAUSE_DURATION)
 
+        # stop if all 'bytes' have been 'downloaded'
         if bytes_downloaded == DOWNLOAD_SIZE:
             break
 
@@ -36,16 +38,40 @@ def main():
 
 def get_progress_bar(bytes_got, total_bytes):
     """Returns a representation of the full progress bar, with all details attached"""
-    num_of_bars = int((bytes_got / total_bytes) * BAR_WIDTH)
-    empty_space = str(' ' * (BAR_WIDTH - num_of_bars))
-    download_percent = get_percent_downloaded(bytes_got, total_bytes)
-    return f'[{ONE_BAR * num_of_bars}{empty_space}] {download_percent} ({bytes_got}/{total_bytes})'
+    num_of_blocks = int((bytes_got / total_bytes) * BAR_WIDTH)
+    percent_got = get_percent_downloaded(bytes_got, total_bytes)
+    remaining_line = f'{ONE_DASH}' * (BAR_WIDTH - num_of_blocks)
+    blocks_got = get_blocks_downloaded(num_of_blocks)
+    fractional_progress = get_progress_as_fraction(bytes_got, total_bytes)
+    start_cap, end_cap = get_bar_caps()
+    return f'{start_cap}{blocks_got}{remaining_line}{end_cap} {percent_got} {fractional_progress}'
+
+
+def get_blocks_downloaded(num_of_blocks):
+    """Returns blocks representing how much 'data' has been 'downloaded'"""
+    block_color = f'[green]'
+    return f'{block_color}{ONE_BAR}{block_color}' * num_of_blocks
 
 
 def get_percent_downloaded(bytes_got, total_bytes):
     """Returns the percentage of bytes downloaded"""
+    percent_color = f'[green]'
     percent = round((bytes_got / total_bytes) * 100, 1)
-    return str(percent) + '%'
+    return f'{percent_color}{percent}{percent_color}%'
+
+
+def get_progress_as_fraction(bytes_got, total_bytes):
+    """Returns, as a fraction, the download progress"""
+    frac_color = f'[grey74]'
+    return f'{frac_color}({bytes_got}/{total_bytes}){frac_color}'
+
+
+def get_bar_caps():
+    """Returns the end caps that are at the beginning and end of the download bar"""
+    cap_color = '[grey100]'
+    left_cap = f'{cap_color}[{cap_color}'
+    right_cap = f'{cap_color}]{cap_color}'
+    return (left_cap, right_cap)
 
 
 if __name__ == '__main__':
